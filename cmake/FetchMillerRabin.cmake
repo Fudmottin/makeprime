b588178b86015d66da662f1cf62abacf8e1caf29
Fudmottin <Fudmottin@gmail.com>
2025-05-09 23:03:04 -0400

    Clean up CMake warnings

--- cmake/FetchMillerRabin.cmake
+++ cmake/FetchMillerRabin.cmake
@@ -1,5 +1,7 @@
 include(FetchContent)
 
+cmake_policy(SET CMP0169 NEW)  # Suppress deprecated usage warning
+
 FetchContent_Declare(
     miller_rabin
     GIT_REPOSITORY https://github.com/Fudmottin/Miller_Rabin.git
@@ -8,5 +10,5 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(miller_rabin)
 
-set(MILLER_RABIN_SOURCE_DIR ${miller_rabin_SOURCE_DIR})
+set(MILLER_RABIN_SOURCE_DIR "${miller_rabin_SOURCE_DIR}")
 
