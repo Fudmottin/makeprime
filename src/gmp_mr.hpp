663d58344036d61d35882f1321883875f9f50bbd
Fudmottin <Fudmottin@gmail.com>
2025-09-21 16:13:45 -0400

    Use a 1000 prime sieve and remove unused files.

--- src/gmp_mr.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <gmpxx.h>
-
-bool miller_rabin_gmp(const mpz_class& n, int rounds, gmp_randclass& rng);
-
---
+++ src/prime_gen.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <array>
+#include <vector>
+
+constexpr bool is_prime(int n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+constexpr std::array<int, 1000> generate_primes() {
+    std::array<int, 1000> primes{};
+    int count = 0;
+    int num = 2;
+    
+    while (count < 1000) {
+        if (is_prime(num)) {
+            primes[count++] = num;
+        }
+        ++num;
+    }
+    return primes;
+}
+
+int main() {
+    constexpr auto small_primes = generate_primes();
+    
+    std::cout << "static const std::array<int, 1000> small_primes = {\n    ";
+    for (size_t i = 0; i < small_primes.size(); ++i) {
+        std::cout << small_primes[i];
+        if (i < small_primes.size() - 1) {
+            std::cout << ", ";
+        }
+        if ((i + 1) % 20 == 0 && i < small_primes.size() - 1) {
+            std::cout << "\n    ";
+        }
+    }
+    std::cout << "\n};\n";
+    
+    return 0;
+}
+
